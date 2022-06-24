a7cbd7ece1238ed1c3d0096ea3771d9bedf6a682
Gychem Baert <gychem.baert@hotmail.com>
2022-06-24 10:25:07 +0200

    Add quanity for products

--- form-view.php
+++ form-view.php
@@ -15,8 +15,7 @@
 <body>
 <div class="container">
     <h1>Place your order</h1>
-    <?php // Navigation for when you need it ?>
-    <?php /*
+
     <nav>
         <ul class="nav">
             <li class="nav-item">
@@ -27,12 +26,12 @@
             </li>
         </ul>
     </nav>
-    */ ?>
+
     <form method="post">
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="email">E-mail:</label>
-                <input type="email" id="email" name="email" class="form-control" value="<?= empty($_POST['email']) ? '' : $_POST['email'] ?>"/>
+                <input type="email" id="email" name="email" class="form-control" value="<?= empty($_POST['email']) ? $_SESSION['email'] : $_POST['email'] ?>"/>
             </div>
             <div></div>
         </div>
@@ -43,21 +42,21 @@
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="street">Street:</label>
-                    <input type="text" name="street" id="street"  class="form-control" value="<?= empty($_POST['street']) ? '' : $_POST['street'] ?>">
+                    <input type="text" name="street" id="street"  class="form-control" value="<?= empty($_POST['street']) ? $_SESSION['street'] : $_POST['street'] ?>">
                 </div>
                 <div class="form-group col-md-6">
                     <label for="streetnumber">Street number:</label>
-                    <input type="number" id="streetnumber" name="streetnumber" class="form-control" value="<?= empty($_POST['streetnumber']) ? '' : $_POST['streetnumber'] ?>">
+                    <input type="number" id="streetnumber" name="streetnumber" class="form-control" value="<?= empty($_POST['streetnumber']) ? $_SESSION['streetnumber']  : $_POST['streetnumber'] ?>">
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="city">City:</label>
-                    <input type="text" id="city" name="city" class="form-control" value="<?= empty($_POST['city']) ? '' : $_POST['city'] ?>">
+                    <input type="text" id="city" name="city" class="form-control" value="<?= empty($_POST['city']) ? $_SESSION['city']  : $_POST['city'] ?>">
                 </div>
                 <div class="form-group col-md-6">
                     <label for="zipcode">Zipcode</label>
-                    <input type="text" id="zipcode" name="zipcode" class="form-control" value="<?= empty($_POST['zipcode']) ? '' : $_POST['zipcode'] ?>">
+                    <input type="text" id="zipcode" name="zipcode" class="form-control" value="<?= empty($_POST['zipcode']) ? $_SESSION['zipcode']  : $_POST['zipcode'] ?>">
                 </div>
             </div>
         </fieldset>
@@ -65,10 +64,14 @@
         <fieldset>
             <legend>Products</legend>
             <?php foreach ($products as $i => $product): ?>
-                <label>
+                <label class="d-flex">
 					<?php // <?= is equal to <?php echo ?>
                     <input type="checkbox" value="1" <?= empty($_POST['products'][$i]) ? '' : 'checked' ?> name="products[<?php echo $i ?>]"/> <?php echo $product['name'] ?> -
-                    &euro; <?= number_format($product['price'], 2) ?></label><br />
+                    &euro; <?= number_format($product['price'], 2) ?>
+                    <input type="number" id="quantity" name="quantity[<?php echo $i ?>]" class="form-control w-25"  value="5">
+                </label>
+                   
+                    <br />
             <?php endforeach; ?>
         </fieldset>
 
--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
-
 declare(strict_types=1); // This line makes PHP behave in a more strict way
-
-// We are going to use session variables so we need to enable sessions
 session_start();
 
-// Use this function when you need to need an overview of these variables
-function whatIsHappening() {
+function whatIsHappening() { // Use this function when you need to need an overview of these variables
     echo '<h2>$_GET</h2>';
     var_dump($_GET);
     echo '<h2>$_POST</h2>';
@@ -16,11 +12,8 @@ function whatIsHappening() {
     echo '<h2>$_SESSION</h2>';
     var_dump($_SESSION);
 }
-
 //whatIsHappening();
-//print_r('<br>-------------------------------------------------');
 
-// TODO: provide some products (you may overwrite the example)
 $products = [
     ['name' => 'BlazeBass Audio Drum Kit', 'price' => 11.99],
     ['name' => 'BlazeBass Audio FX Effects', 'price' => 11.99],
@@ -28,45 +21,72 @@ $products = [
     ['name' => 'BlazeBass Audio Vocal Leads and Hooks', 'price' => 11.99],
 ];
 
+if (empty($_SESSION['email'])) {
+    $_SESSION["email"] = '';
+} 
+if (empty($_SESSION['street'])) {
+    $_SESSION["street"] = '';
+} 
+if (empty($_SESSION['streetnumber'])) {
+    $_SESSION["streetnumber"] = '';
+} 
+if (empty($_SESSION['city'])) {
+    $_SESSION["city"] = '';
+} 
+if (empty($_SESSION['zipcode'])) {
+    $_SESSION["zipcode"] = '';
+} 
+
 $totalValue = 0;
-print_r('<br><b>[ERROR]</b>');
+
 function validate()
 {
-    // TODO: This function will send a list of invalid fields back
     $validateArray = [];
+    print_r('<br><b>[ERROR]</b>');
     print_r('<br>');
-    if (empty($_POST['email'])) {
+
+    if (empty($_POST['email'])) { // Email Validation
         array_push($validateArray, 'Email');
     } else {
         if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $_SESSION["email"] = $_POST['email'];
         } else {
             echo "- Email address is invalid ! Enter a valid email address, for example: yourname@website.com<br>";
         }
     }
 
-    if (empty($_POST['street'])) {
+    if (empty($_POST['street'])) { // Street Validation
         array_push($validateArray, 'Street');
+    } else {
+        $_SESSION["street"] = $_POST['street'];
     }
-    if (empty($_POST['streetnumber'])) {
+
+    if (empty($_POST['streetnumber'])) { // Streetnumber Validation
         array_push($validateArray, 'Street number'); 
     } else {
         if(is_numeric($_POST['streetnumber'])) {
+            $_SESSION["streetnumber"] = $_POST['streetnumber'];
         } else {
             echo "- Street number is invalid !<br>";
         }
     }
-    if (empty($_POST['city'])) {
+    if (empty($_POST['city'])) { // City Validation
         array_push($validateArray, 'City'); 
+    } else {
+        $_SESSION["city"] = $_POST['city'];
     }
-    if (empty($_POST['zipcode'])) {
+
+    if (empty($_POST['zipcode'])) { // Zipcode Validation
         array_push($validateArray, 'Zipcode'); 
     } else {
         if(is_numeric($_POST['zipcode'])) {
+            $_SESSION["zipcode"] = $_POST['zipcode'];
         } else {
             echo "- Zipcode is invalid !<br>";
         }
     }
-    if (empty($_POST['products'])) {
+
+    if (empty($_POST['products'])) { // Products Validation
         array_push($validateArray, 'Products'); 
     }
     if(!empty($validateArray)) {
@@ -106,7 +126,9 @@ function handleForm()
 }
 
 function loadOrder($products) {
-    $_SESSION["productName"] = $products[0]['name'] . ' ' . $products[0]['price'];
+
+
+    //$_SESSION["productName"] = $products[0]['name'] . ' ' . $products[0]['price'];
     print_r('<br><br><b><u>Thanks for placing your order, you can find the order details below.</u></b><br><br>');
 
     print_r('<b>Email</b> ' . $_POST['email']);
@@ -114,13 +136,15 @@ function loadOrder($products) {
     print_r('<br><b>Zipcode & City</b> ' . $_POST['zipcode'] . ' ' . $_POST['city']);
 
     $orderedProducts = $_POST['products'];
+
     $totalPrice = 0;
     print_r('<br><br><b>Products:</b><br>');
     foreach ($orderedProducts as $i => $product): 
-        $orderedProduct = $products[$i]['name'] . ' ' . $products[$i]['price'];
+        $productTotalPrice = $products[$i]['price'] * $_POST['quantity'][$i];
+        $orderedProduct = $products[$i]['name'] . ' €' . $products[$i]['price'] . ' [Quantity ' . $_POST['quantity'][$i] . '] [Total Price: ' . $productTotalPrice . ']';
         print_r($orderedProduct);
         print_r('<br>');
-        $totalPrice = $totalPrice + $products[$i]['price']; 
+        $totalPrice = $totalPrice + $productTotalPrice; 
     endforeach; 
 
     print_r('<br><b>Total Price</b> ' . $totalPrice);
